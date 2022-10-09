440b6769c73c0ebbd54528d14ccc4da283df6bc5
questina <chrismatis123@gmail.com>
2022-10-10 00:53:49 +0300

    added 6 hw

---
+++ 06/lru_cache.py
@@ -0,0 +1,59 @@
+class CacheNode:
+    def __init__(self, node_key, node_val):
+        self.key = node_key
+        self.val = node_val
+        self.next = None
+        self.prev = None
+
+
+class LRUCache:
+
+    def __init__(self, limit=42):
+        self.cache_dict = {}
+        self.limit = limit
+        self.recent_elem = None
+        self.last_elem = None
+        pass
+
+    def get(self, key):
+        if key in self.cache_dict:
+            cache_node = self.cache_dict[key]
+            if len(self.cache_dict) == 1:
+                return self.cache_dict[key].val
+            if cache_node.key == self.last_elem.key:
+                self.last_elem.next.prev, self.last_elem = None, self.last_elem.next
+            elif cache_node.key == self.recent_elem.key:
+                return self.cache_dict[key].val
+            else:
+                cache_node.prev.next = cache_node.next
+                cache_node.next.prev = cache_node.prev
+            cache_node.prev = self.recent_elem
+            self.recent_elem.next = cache_node
+            self.recent_elem = cache_node
+            cache_node.next = None
+            return self.cache_dict[key].val
+        else:
+            return None
+
+    def set(self, key, value):
+        new_node = CacheNode(key, value)
+        if self.last_elem is None:
+            self.recent_elem = new_node
+            self.last_elem = new_node
+            self.cache_dict[key] = new_node
+        else:
+            if key in self.cache_dict:
+                self.cache_dict[key].val = value
+                self.get(key)
+            else:
+                if len(self.cache_dict) == self.limit:
+                    self.cache_dict.pop(self.last_elem.key, None)
+                    if self.last_elem.next is None:
+                        self.last_elem = new_node
+                    else:
+                        self.last_elem = self.last_elem.next
+                    self.last_elem.prev = None
+                self.recent_elem.next = new_node
+                new_node.prev = self.recent_elem
+                self.recent_elem = new_node
+                self.cache_dict[key] = new_node
---
+++ 06/tests.py
@@ -0,0 +1,39 @@
+import unittest
+from lru_cache import LRUCache
+
+
+class TestLRUCache(unittest.TestCase):
+    def test_small_cache(self):
+        cache = LRUCache(2)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+
+        assert cache.get("k3") is None
+        assert cache.get("k2") == "val2"
+        assert cache.get("k1") == "val1"
+
+        cache.set("k3", "val3")
+
+        assert cache.get("k3") == "val3"
+        assert cache.get("k2") is None
+        assert cache.get("k1") == "val1"
+
+    def test_big_cache(self):
+        cache = LRUCache(50)
+        for i in range(50):
+            cache.set(f"k_{i}", f"val_{i}")
+        for _ in range(50):
+            cache.set("k_1", "val_2")
+        assert cache.get("k_3") == "val_3"
+        assert cache.get("k_1") == "val_2"
+        cache.set("k_50", "val_50")
+        assert cache.get("k_0") is None
+        assert cache.get("k_50") is "val_50"
+
+    def test_one_size_cache(self):
+        cache = LRUCache(1)
+        cache.set("k_1", "val_1")
+        assert cache.get("k_1") == "val_1"
+        cache.set("k_2", "val_2")
+        assert cache.get("k_1") is None
+        assert cache.get("k_2") == "val_2"
